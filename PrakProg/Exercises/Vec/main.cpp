aaaba069358d10f8e9d24fc5d6d926f10e984df8
Alfredos-man <alfred.riis.lykke@gmail.com>
2026-02-19 08:12:51 +0100

    Vec

--- PrakProg/Exercises/Vec/main.cpp
+++ PrakProg/Exercises/Vec/main.cpp
@@ -3,13 +3,15 @@
 
 
 int main(){
+        std::cout << "init vektor 1" << std::endl;
         vec v1(1,2,3);
         v1.printVec();
 
+        std::cout << "init vektor 1" << std::endl;
         vec v2(3,2,1);
         v2.printVec();
 
-        
+        std::cout << "plus dem sammen" << std::endl;
         vec sumvec = vecPlus(v1, v2);
         sumvec.printVec();
         
