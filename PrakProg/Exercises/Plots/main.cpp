3858139460c2d6d6aea120267bfb7b05b7f1c6f2
Alfredos-man <alfred.riis.lykke@gmail.com>
2026-03-26 13:20:20 +0100

    Plot done

--- PrakProg/Exercises/Plots/main.cpp
+++ PrakProg/Exercises/Plots/main.cpp
@@ -2,21 +2,53 @@
 #include <iostream>
 #include <string>
 
-int main(int argc,char** argv){
-	double xmin=-3,xmax=3,dx=0.1;
-	for(int i=0;i<argc;i++){
-		std::string arg=argv[i];
-		std::cerr<<"i= "<<i<<" arg="<<arg<<"\n";
-		if(arg=="-xmin" && i+1<argc)xmin=std::stod(argv[i+1]);
-		if(arg=="-xmax" && i+1<argc)xmax=std::stod(argv[i+1]);
-		if(arg=="-dx" && i+1<argc)dx=std::stod(argv[i+1]);
-		}
-	std::cerr<<"xmin= "<<xmin<<"\n";
-	std::cerr<<"xmax= "<<xmax<<"\n";
-	std::cerr<<"dx= "<<dx<<"\n";
-	std::cout<<std::scientific;
-	for(double x=xmin;x<=xmax;x+=dx){
-		std::cout<<x<<" "<<myerf(x)<<" "<<sgamma(x)<< " " << lngamma(x)<< std::endl;
-	}
-	return 0;
+int main(int argc, char** argv) {
+    double xmin = -5, xmax = 10, dx = 0.01;
+    std::string func = "erf";
+
+    for (int i = 1; i < argc; i++) {
+        std::string arg = argv[i];
+
+        if (arg == "-xmin" && i + 1 < argc) {
+            xmin = std::stod(argv[++i]);
+        }
+        else if (arg == "-xmax" && i + 1 < argc) {
+            xmax = std::stod(argv[++i]);
+        }
+        else if (arg == "-dx" && i + 1 < argc) {
+            dx = std::stod(argv[++i]);
+        }
+        else if (arg == "-func" && i + 1 < argc) {
+            func = argv[++i];
+        }
+    }
+
+    std::cerr << "xmin = " << xmin << "\n";
+    std::cerr << "xmax = " << xmax << "\n";
+    std::cerr << "dx   = " << dx << "\n";
+    std::cerr << "func = " << func << "\n";
+
+    std::cout << std::scientific;
+
+    if (func == "erf") {
+        for (double x = xmin; x <= xmax; x += dx) {
+            std::cout << x << " " << myerf(x) << "\n";
+        }
+    }
+    else if (func == "sgamma") {
+        for (double x = xmin; x <= xmax; x += dx) {
+            std::cout << x << " " << sgamma(x) << "\n";
+        }
+    }
+    else if (func == "lngamma") {
+        for (double x = xmin; x <= xmax; x += dx) {
+            std::cout << x << " " << lngamma(x) << "\n";
+        }
+    }
+    else {
+        std::cerr << "Ukendt funktion: " << func << "\n";
+        return 1;
+    }
+
+    return 0;
 }
