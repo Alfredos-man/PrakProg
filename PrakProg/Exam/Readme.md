ab57690ba54ee352767c7d392e70bafac6071842
Alfredos-man <alfred.riis.lykke@gmail.com>
2026-06-29 22:57:41 +0200

    Exam

---
+++ PrakProg/Exam/Readme.md
@@ -0,0 +1,77 @@
+# Question 6: "Symmetric rank-1 update of a size-n symmetric eigenvalue problem"
+Alfred Riis Lykke 
+
+
+## Overview
+The following code is made to find the eigenvalues of a matrix A on the form:
+```
+A = D + u*u^T
+```
+
+where D is a diagonal Matrix and u is a vector
+This algorithm is meant to run in O(n^2) time.
+
+When running "make", 2 things will happen:
+1. The file Out.txt will be generated containing test of the algorithm on various cases 
+2. A benchmark will begin to run the algorithm with increasing size starting from n=100 up to n=5000 with stepsize 100. This generates the file "benchmark.data" used to make a plot over the runtime.
+
+## Method/theory
+
+The eigenvalues are obtained by solving the secular equation as described in the book
+$$
+1 + \sum_{i=1}^{n} \frac{u_i^2}{d_i - \lambda} = 0
+$$
+The algorithm performs the following steps:
+
+1. Sort the diagonal entries of D. 
+2. Reorder the corresponding entries of u.
+2. Use the interlacing property to determine an interval containing each eigenvalue.
+3. Solve the secular equation in each interval using the bisection method.
+4. Return the computed eigenvalues.
+
+## Out.txt
+The file Out.txt shows some testcases of this algorithm where the input d denotes the Diagonal matrix. For example d = 1 3 5 coresponds to the matrix: 
+$$
+D=
+\begin{bmatrix}
+1&0&0\\
+0&3&0\\
+0&0&5
+\end{bmatrix}
+$$
+and u = 1 2 1 is:
+
+$$
+u=
+\begin{bmatrix}
+1&2&1\\
+\end{bmatrix} ^T
+$$
+The found eigenvalues are written under "Eigenvalues"
+
+
+
+
+## Benchmark.png
+The plot, "benchmark.png" shows the scaling runtime of O(n^2). This is done by timing the runtime for a given n where the size of both matrixes is 
+$$
+D = (d_1 ... d_n) 
+$$
+and 
+$$
+u = (u_1 ... u_n)
+$$
+I then do a fit of this data using 
+$$
+f(n)=an^2
+$$
+where a is just a verry small number.
+
+The plot shows that the runtime scales almost perfectly with O(n^2)
+## Conclution
+The rank-1 eigenvalue algoritm has been implimented as acording to the task and the book, and all points of the task has been concidered, along with a plot showing the intended runtime. 
+
+score: 10/10
+
+
+
--- PrakProg/Exam/main.cpp
+++ PrakProg/Exam/main.cpp
@@ -1,9 +1,14 @@
+
 #include "rank.h"
 
-#include <iostream>
-#include <vector>
+#include <chrono>
+#include <fstream>
 #include <iomanip>
+#include <iostream>
+#include <random>
 #include <stdexcept>
+#include <string>
+#include <vector>
 
 void runTest(const std::vector<double>& d,
              const std::vector<double>& u,
@@ -15,61 +20,125 @@ void runTest(const std::vector<double>& d,
         auto eig = rank1Eigenvalues(d, u);
 
         std::cout << std::setprecision(15);
+
         std::cout << "d: ";
-        for (double x : d) std::cout << x << " ";
+        for (double x : d)
+            std::cout << x << " ";
+
         std::cout << "\n";
 
         std::cout << "u: ";
-        for (double x : u) std::cout << x << " ";
+        for (double x : u)
+            std::cout << x << " ";
+
         std::cout << "\n";
 
-        std::cout << "eigenvalues:\n";
-        for (double x : eig) {
+        std::cout << "Eigenvalues:\n";
+        for (double x : eig)
             std::cout << x << "\n";
-        }
     }
-    catch (const std::exception& e) {
+    catch (const std::exception& e)
+    {
         std::cout << "ERROR: " << e.what() << "\n";
     }
 }
 
-int main()
+void runBenchmark()
 {
-    runTest(
-        {1.0, 3.0, 5.0},
-        {1.0, 2.0, 1.0},
-        "Basic 3x3"
-    );
+    std::ofstream file("benchmark.data");
 
-    runTest(
-        {1.0, 2.0, 4.0, 8.0},
-        {1.0, 1.0, 1.0, 1.0},
-        "All ones u"
-    );
+    std::mt19937 gen(42);
+    std::uniform_real_distribution<double> dist(0.0,1.0);
 
-    runTest(
-        {10.0},
-        {3.0},
-        "1x1 case"
-    );
+    //std::cout << "test at den kører kompilere rigtigt" << std::endl;
 
-    runTest(
-        {5.0, 1.0, 3.0},
-        {1.0, 1.0, 2.0},
-        "Unsorted d"
-    );
+    for(int n=100; n<=5000; n+=100)
+    {
+        std::vector<double> d(n);
+        std::vector<double> u(n);
 
-    runTest(
-        {-2.0, 0.0, 4.0},
-        {1.0, 2.0, 3.0},
-        "Negative and positive d"
-    );
+        for(int i=0;i<n;i++)
+        {
+            d[i]=i+1;
+            u[i]=dist(gen);
+        }
 
-    runTest(
-        {1.0, 10.0, 100.0},
-        {0.1, 0.2, 0.3},
-        "Small update"
-    );
+        auto start =
+            std::chrono::high_resolution_clock::now();
+
+        rank1Eigenvalues(d,u);
+
+        auto stop =
+            std::chrono::high_resolution_clock::now();
+
+        double t =
+            std::chrono::duration<double,std::milli>(stop-start).count();
+
+        file << n << " "
+             << t << " "
+             << n*n << "\n";
+
+        std::cout << "n = "
+                  << n
+                  << " finished\n";
+    }
+
+    std::cout << "Benchmark written to benchmark.data\n";
+}
+
+int main(int argc, char* argv[])
+{
+    if(argc != 2)
+    {
+        std::cout << "Usage:\n";
+        std::cout << "./rank opg1\n";
+        std::cout << "./rank plot\n";
+        return 1;
+    }
+
+    std::string mode = argv[1];
+
+    if(mode == "-tests")
+    {
+        runTest(
+            {1.0,3.0,5.0},
+            {1.0,2.0,1.0},
+            "Basic 3x3");
+
+        runTest(
+            {1.0,2.0,4.0,8.0},
+            {1.0,1.0,1.0,1.0},
+            "All ones u");
+
+        runTest(
+            {10.0},
+            {3.0},
+            "1x1");
+
+        runTest(
+            {5.0,1.0,3.0},
+            {1.0,1.0,2.0},
+            "Unsorted d");
+
+        runTest(
+            {-2.0,0.0,4.0},
+            {1.0,2.0,3.0},
+            "Negative d");
+
+        runTest(
+            {1.0,10.0,100.0},
+            {0.1,0.2,0.3},
+            "Small update");
+    }
+    else if(mode == "-plot")
+    {
+        runBenchmark();
+    }
+    else
+    {
+        std::cout << "Unknown option: " << mode << "\n";
+    }
 
     return 0;
 }
+
